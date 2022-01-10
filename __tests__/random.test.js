ae1f14c0d069d57d9594f0fb34283274513ebb43
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-10 15:13:33 -0600

    Adding the failing test

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -2,5 +2,6 @@
 const checkIfEqual = require('../lib/random.js');
 
 test('checks if 10 is equal to 10', () => {
-    expect(checkIfEqual(10,10)).toBe(true);
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
 });
