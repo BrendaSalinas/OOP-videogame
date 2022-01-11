7dd3ce65921dbc5787fec0947ab3fa0dd62a89c9
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-10 18:53:08 -0600

    running the failed tests

--- __tests__/random.test.js
+++
@@ -1,7 +0,0 @@
-
-const checkIfEqual = require('../lib/random.js');
-
-test('checks if 10 is equal to 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
