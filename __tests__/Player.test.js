afde9752a75a0abb8de2de3249ab3275f1d92443
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-11 06:11:58 -0600

    write failing test for getStats and getInventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,3 @@
-
 const { expect } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 
@@ -6,7 +5,7 @@ const Player = require('../lib/Player.js');
 const Potion = require('../lib/__mocks__/Potion');
 
 test('creates a player object', () => {
-    const player = new Player('Dave');
+    const player = new Player('Brenda');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
@@ -15,3 +14,20 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
+test('gets a players stats as and object', () => {
+    const player = new Player('Brenda');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Brenda');
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
